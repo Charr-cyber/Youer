10c76deb1f4dc6b5f826e7b24995beb60385b790
Charr-cyber <charweell2@gmail.com>
2025-09-23 10:32:24 +0300

    FIX2

--- src/main/java/com/mohistmc/youer/bukkit/pluginfix/PluginFixManager.java
+++ src/main/java/com/mohistmc/youer/bukkit/pluginfix/PluginFixManager.java
@@ -2,15 +2,18 @@ package com.mohistmc.youer.bukkit.pluginfix;
 
 import com.mohistmc.youer.Youer;
 import java.util.function.Consumer;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import org.bukkit.entity.TeleportFlag;
+
+import org.objectweb.asm.*;
 import org.objectweb.asm.tree.*;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.Opcodes;
 
 import static org.objectweb.asm.Opcodes.ARETURN;
 
@@ -19,83 +22,50 @@ public class PluginFixManager {
     // -------------------- TELEPORT --------------------
 
     /**
-     * Oyuncuyu dungeon içine güvenli şekilde teleport eder.
-     * MythicDungeons'ın forceTeleport2 metodunu replacement'ı.
+     * Sync / Async teleport yapar.
+     * Paper API varsa async, yoksa sync + chunk load
      */
-    public static void teleportEntityToDungeon(Entity entity, Location target) {
-        if (entity == null || target == null) return;
-        
-        // Entity Player mı kontrol et
-        if (!(entity instanceof Player player)) {
-            return; // Sadece Player'ları teleport et
-        }
+    public static void teleportForce(Entity entity, Location location) {
+        if (entity == null || location == null) return;
 
-        // Bukkit API sadece ana thread'de güvenli → sync task kullanıyoruz
-        Bukkit.getScheduler().runTask(
-                Bukkit.getPluginManager().getPlugin("MythicDungeons"),
-                () -> {
-                    try {
-                        // Chunk'ın yüklü olduğundan emin ol
-                        if (!target.getChunk().isLoaded()) {
-                            target.getChunk().load();
-                        }
-                        
-                        // Güvenli teleport
-                        player.teleport(target);
-                    } catch (Exception e) {
-                        System.err.println("[MythicDungeons Patch] Teleport failed for player " + player.getName());
-                        e.printStackTrace();
+        Plugin mythicPlugin = Bukkit.getPluginManager().getPlugin("MythicDungeons");
+        if (mythicPlugin == null) return;
+
+        Bukkit.getScheduler().runTask(mythicPlugin, () -> {
+            try {
+                if (Bukkit.getPluginManager().isPluginEnabled("Paper")) {
+                    if (entity instanceof Player player) {
+                        player.teleportAsync(location, TeleportCause.PLUGIN,
+                                TeleportFlag.EntityState.RETAIN_PASSENGERS)
+                              .thenAccept(success -> {});
+                        return;
                     }
                 }
-        );
+
+                // Sync teleport (Paper yoksa)
+                Chunk chunk = location.getChunk();
+                if (!chunk.isLoaded()) chunk.load();
+                entity.teleport(location);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    /**
-     * Geriye uyumluluk için eski metod (kullanılmıyor artık)
-     * @deprecated Use teleportEntityToDungeon instead
-     */
-    @Deprecated
-    public static void teleportPlayerToDungeon(Player player, Location target) {
-        teleportEntityToDungeon(player, target);
-    }
-
-    /**
-     * ASM patch: MythicDungeons Util.forceTeleport2 metodunu replacement eder.
-     */
-    public static void patchDungeonTeleport(ClassNode node) {
+    // ASM patch: forceTeleport2 çağrılarını yakalar
+    public static void patchForceTeleport(ClassNode node) {
         for (MethodNode method : node.methods) {
-            // MythicDungeons'ın gerçek teleport metodu
-            if (method.name.equals("forceTeleport2")) {
-                // Method signature kontrolü
-                if (method.desc.equals("(Lorg/bukkit/entity/Entity;Lorg/bukkit/Location;)V")) {
-                    
-                    // Method'ın tamamını sil ve yenisini koy
-                    method.instructions.clear();
-                    method.tryCatchBlocks.clear();
-                    
-                    InsnList newCode = new InsnList();
-                    
-                    // Parametreleri stack'e yükle
-                    newCode.add(new VarInsnNode(Opcodes.ALOAD, 0)); // Entity entity
-                    newCode.add(new VarInsnNode(Opcodes.ALOAD, 1)); // Location location
-                    
-                    // Kendi metodumuzu çağır
-                    newCode.add(new MethodInsnNode(
-                        Opcodes.INVOKESTATIC,
-                        Type.getInternalName(PluginFixManager.class),
-                        "teleportEntityToDungeon",
-                        "(Lorg/bukkit/entity/Entity;Lorg/bukkit/Location;)V",
-                        false
-                    ));
-                    
-                    // Return
-                    newCode.add(new InsnNode(Opcodes.RETURN));
-                    
-                    // Yeni kodu method'a ata
-                    method.instructions = newCode;
-                    
-                    System.out.println("[MythicDungeons Patch] Successfully patched Util.forceTeleport2");
-                    break; // Metodu bulduk, döngüden çık
+            for (AbstractInsnNode insn : method.instructions) {
+                if (insn instanceof MethodInsnNode mInsn) {
+                    if (mInsn.owner.equals("net/playavalon/mythicdungeons/utility/helpers/Util")
+                            && mInsn.name.equals("forceTeleport2")
+                            && mInsn.desc.equals("(Lorg/bukkit/entity/Entity;Lorg/bukkit/Location;)V")) {
+                        mInsn.setOpcode(Opcodes.INVOKESTATIC);
+                        mInsn.owner = Type.getInternalName(PluginFixManager.class);
+                        mInsn.name = "teleportForce";
+                        mInsn.desc = "(Lorg/bukkit/entity/Entity;Lorg/bukkit/Location;)V";
+                    }
                 }
             }
         }
@@ -107,7 +77,8 @@ public class PluginFixManager {
         if (plugin.equals("WorldEdit")) {
             String adapter = System.getProperty("worldedit.bukkit.adapter");
             if (adapter == null) {
-                System.setProperty("worldedit.bukkit.adapter", "com.sk89q.worldedit.bukkit.adapter.impl.v1_21.PaperweightAdapter");
+                System.setProperty("worldedit.bukkit.adapter",
+                        "com.sk89q.worldedit.bukkit.adapter.impl.v1_21.PaperweightAdapter");
             }
         }
 
@@ -124,16 +95,12 @@ public class PluginFixManager {
             case "com.onarandombox.MultiverseCore.utils.WorldManager" -> {
                 return patch(clazz, MultiverseCore::fix);
             }
-            // MythicDungeons patch - DOĞRU CLASS
-            case "net.playavalon.mythicdungeons.utility.helpers.Util" -> {
-                System.out.println("[MythicDungeons Patch] Patching Util class...");
-                return patch(clazz, PluginFixManager::patchDungeonTeleport);
-            }
         }
 
-        // CMI patch'leri
         if (className.startsWith("net.Zrips.CMILib.") || className.startsWith("com.Zrips.CMI.")) {
-            return patch(clazz, node -> helloWorld(node, "net.minecraft.server.network.PlayerConnection", "net.minecraft.server.network.ServerGamePacketListenerImpl"));
+            return patch(clazz, node -> helloWorld(node,
+                    "net.minecraft.server.network.PlayerConnection",
+                    "net.minecraft.server.network.ServerGamePacketListenerImpl"));
         }
 
         Consumer<ClassNode> patcher = switch (className) {
@@ -143,7 +110,9 @@ public class PluginFixManager {
             };
             case "net.Zrips.CMILib.Reflections" -> node -> helloWorld(node, "bR", "f_36096_");
             case "net.Zrips.CMILib.RawMessages.RawMessageManager" ->
-                    node -> helloWorld(node, "net.minecraft.server.network.PlayerConnection", "net.minecraft.server.network.ServerGamePacketListenerImpl");
+                    node -> helloWorld(node,
+                            "net.minecraft.server.network.PlayerConnection",
+                            "net.minecraft.server.network.ServerGamePacketListenerImpl");
             case "com.sk89q.worldedit.bukkit.BukkitConfiguration" -> node -> {
                 helloWorld(node, "I accept that I will receive no support with this flag enabled.", Youer.modid);
                 helloWorld(node, "allow-editing-on-unsupported-versions", Youer.modid);
@@ -162,19 +131,22 @@ public class PluginFixManager {
             case "org.mvplugins.multiverse.external.paperlib.PaperLib",
                  "me.SuperRonanCraft.BetterRTP.lib.paperlib.PaperLib",
                  "com.plotsquared.bukkit.paperlib.PaperLib" -> PluginFixManager::removePaper0;
+            case "net.playavalon.mythicdungeons.utility.helpers.Util" -> PluginFixManager::patchForceTeleport; // MythicDungeons patch
             default -> null;
         };
 
         return patcher == null ? clazz : patch(clazz, patcher);
     }
 
-    // -------------------- ASM HELPER --------------------
+    // -------------------- HELPER METHODS --------------------
 
     private static void removePaper(ClassNode node) {
         for (MethodNode methodNode : node.methods) {
             if (methodNode.name.equals("hasPaperAsyncSupport") && methodNode.desc.equals("()Z")) {
                 InsnList toInject = new InsnList();
-                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(PluginFixManager.class), "hasPaperAsyncSupport", "()Z", false));
+                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+                        Type.getInternalName(PluginFixManager.class),
+                        "hasPaperAsyncSupport", "()Z"));
                 toInject.add(new InsnNode(Opcodes.IRETURN));
                 methodNode.instructions = toInject;
             }
@@ -186,34 +158,27 @@ public class PluginFixManager {
         helloWorld(node, "io.papermc.paper.configuration.Configuration", Youer.modid);
     }
 
-    public static boolean hasPaperAsyncSupport() { return false; }
+    public static boolean hasPaperAsyncSupport() {
+        return false;
+    }
 
     private static byte[] patch(byte[] basicClass, Consumer<ClassNode> handler) {
-        try {
-            ClassNode node = new ClassNode();
-            new ClassReader(basicClass).accept(node, 0);
-            handler.accept(node);
-            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-            node.accept(writer);
-            return writer.toByteArray();
-        } catch (Exception e) {
-            System.err.println("[PluginFixManager] Failed to patch class: " + e.getMessage());
-            e.printStackTrace();
-            return basicClass; // Patch başarısızsa orijinali döndür
-        }
+        ClassNode node = new ClassNode();
+        new ClassReader(basicClass).accept(node, 0);
+        handler.accept(node);
+        ClassWriter writer = new ClassWriter(0);
+        node.accept(writer);
+        return writer.toByteArray();
     }
 
     private static void redirectMethodToGetNMSVersion(ClassNode node, String methodName) {
         for (MethodNode methodNode : node.methods) {
             if (methodNode.name.equals(methodName) && methodNode.desc.equals("()Ljava/lang/String;")) {
                 InsnList toInject = new InsnList();
-                toInject.add(new MethodInsnNode(
-                        Opcodes.INVOKESTATIC,
+                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
                         Type.getInternalName(PluginFixManager.class),
                         "getNMSVersion",
-                        "()Ljava/lang/String;",
-                        false
-                ));
+                        "()Ljava/lang/String;"));
                 toInject.add(new InsnNode(ARETURN));
                 methodNode.instructions = toInject;
                 methodNode.tryCatchBlocks.clear();
@@ -222,6 +187,7 @@ public class PluginFixManager {
     }
 
     private static void qs(ClassNode node) { redirectMethodToGetNMSVersion(node, "getNMSVersion"); }
+
     private static void fawe(ClassNode node) { redirectMethodToGetNMSVersion(node, "getPackageVersion"); }
 
     public static void ex(ClassNode node) {
@@ -229,13 +195,10 @@ public class PluginFixManager {
             if (method.name.equals("make") && method.desc.equals("(Ljava/lang/String;)Ljava/lang/String;")) {
                 InsnList toInject = new InsnList();
                 toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                toInject.add(new MethodInsnNode(
-                        Opcodes.INVOKESTATIC,
+                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
                         Type.getInternalName(PluginFixManager.class),
                         "make",
-                        "(Ljava/lang/String;)Ljava/lang/String;",
-                        false
-                ));
+                        "(Ljava/lang/String;)Ljava/lang/String;"));
                 toInject.add(new InsnNode(ARETURN));
                 method.instructions = toInject;
                 method.tryCatchBlocks.clear();
@@ -247,8 +210,8 @@ public class PluginFixManager {
         node.methods.forEach(method -> {
             for (AbstractInsnNode next : method.instructions) {
                 if (next instanceof LdcInsnNode ldcInsnNode) {
-                    if (ldcInsnNode.cst instanceof String str) {
-                        if (a.equals(str)) ldcInsnNode.cst = b;
+                    if (ldcInsnNode.cst instanceof String str && a.equals(str)) {
+                        ldcInsnNode.cst = b;
                     }
                 }
             }
@@ -258,11 +221,23 @@ public class PluginFixManager {
     private static void helloWorld(ClassNode node, int a, int b) {
         node.methods.forEach(method -> {
             for (AbstractInsnNode next : method.instructions) {
-                if (next instanceof IntInsnNode ldcInsnNode) {
-                    if (ldcInsnNode.operand == a) ldcInsnNode.operand = b;
+                if (next instanceof IntInsnNode ldcInsnNode && ldcInsnNode.operand == a) {
+                    ldcInsnNode.operand = b;
                 }
             }
         });
+    }
+
+    private static void replaceReturn(ClassNode node, String methodName, Object idc) {
+        for (MethodNode methodNode : node.methods) {
+            if (methodNode.name.equals(methodName)) {
+                InsnList toInject = new InsnList();
+                toInject.add(new LdcInsnNode(idc));
+                toInject.add(new InsnNode(ARETURN));
+                methodNode.instructions = toInject;
+                methodNode.tryCatchBlocks.clear();
+            }
+        }
     }
 
     public static String getNMSVersion() { return "v1_21_R1"; }
